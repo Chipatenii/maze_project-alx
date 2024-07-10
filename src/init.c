f41ec6fed144a66a8975faeb2de780bb5630376b
Innocent Manda <Innocentmanda70@gmail.com>
2024-07-10 10:22:57 +0200

    Update init.c

--- src/init.c
+++ src/init.c
@@ -1,34 +1,37 @@
 #include "../headers/maze.h"
 
 /**
- * initSDL - Initializes SDL and sets up the game window and renderer.
- * @game: A pointer to the Game structure.
+ * initSDL - Initializes SDL and creates a window and renderer.
+ * @game: Pointer to the game structure.
  *
- * Return: true if successful, false otherwise.
+ * Return: true if initialization is successful, false otherwise.
  */
 bool initSDL(Game *game)
 {
     if (SDL_Init(SDL_INIT_VIDEO) < 0)
     {
-        printf("SDL could not initialize! SDL_Error: %s\n", SDL_GetError());
+        SDL_Log("Unable to initialize SDL: %s", SDL_GetError());
         return false;
     }
 
     game->window = SDL_CreateWindow("Maze Game",
-                                    SDL_WINDOWPOS_UNDEFINED,
-                                    SDL_WINDOWPOS_UNDEFINED,
+                                    SDL_WINDOWPOS_CENTERED,
+                                    SDL_WINDOWPOS_CENTERED,
                                     SCREEN_WIDTH, SCREEN_HEIGHT,
                                     SDL_WINDOW_SHOWN);
-    if (game->window == NULL)
+    if (!game->window)
     {
-        printf("Window could not be created! SDL_Error: %s\n", SDL_GetError());
+        SDL_Log("Could not create window: %s", SDL_GetError());
+        SDL_Quit();
         return false;
     }
 
     game->renderer = SDL_CreateRenderer(game->window, -1, SDL_RENDERER_ACCELERATED);
-    if (game->renderer == NULL)
+    if (!game->renderer)
     {
-        printf("Renderer could not be created! SDL_Error: %s\n", SDL_GetError());
+        SDL_DestroyWindow(game->window);
+        SDL_Log("Could not create renderer: %s", SDL_GetError());
+        SDL_Quit();
         return false;
     }
 
@@ -38,12 +41,13 @@ bool initSDL(Game *game)
 
     if (!game->textures.wall || !game->textures.floor || !game->textures.ceiling)
     {
-        printf("Failed to load textures!\n");
+        SDL_Log("Failed to load textures");
+        closeSDL(game);
         return false;
     }
 
-    game->player.posX = 22.0;
-    game->player.posY = 12.0;
+    game->player.x = 22.0;
+    game->player.y = 12.0;
     game->player.dirX = -1.0;
     game->player.dirY = 0.0;
     game->player.planeX = 0.0;
@@ -53,35 +57,37 @@ bool initSDL(Game *game)
 }
 
 /**
- * loadTexture - Loads a texture from a specified path.
- * @renderer: The SDL renderer.
- * @path: The path to the image file.
+ * loadTexture - Loads a texture from a file.
+ * @renderer: The renderer to use.
+ * @path: The file path to the image.
  *
- * Return: A pointer to the loaded texture, or NULL on failure.
+ * Return: The loaded texture or NULL on failure.
  */
 SDL_Texture *loadTexture(SDL_Renderer *renderer, const char *path)
 {
-    SDL_Texture *newTexture = NULL;
-    SDL_Surface *loadedSurface = SDL_LoadBMP(path);
-    if (loadedSurface == NULL)
+    SDL_Surface *surface = SDL_LoadBMP(path);
+    if (!surface)
     {
-        printf("Unable to load image %s! SDL_Error: %s\n", path, SDL_GetError());
+        SDL_Log("Failed to load image %s: %s", path, SDL_GetError());
+        return NULL;
     }
-    else
+
+    SDL_Texture *texture = SDL_CreateTextureFromSurface(renderer, surface);
+    SDL_FreeSurface(surface);
+
+    if (!texture)
     {
-        newTexture = SDL_CreateTextureFromSurface(renderer, loadedSurface);
-        SDL_FreeSurface(loadedSurface);
-        if (newTexture == NULL)
-        {
-            printf("Unable to create texture from %s! SDL_Error: %s\n", path, SDL_GetError());
-        }
+        SDL_Log("Failed to create texture from %s: %s", path, SDL_GetError());
     }
-    return newTexture;
+
+    return texture;
 }
 
 /**
- * closeSDL - Cleans up SDL resources.
- * @game: A pointer to the Game structure.
+ * closeSDL - Cleans up and closes SDL.
+ * @game: Pointer to the game structure.
+ *
+ * Return: void
  */
 void closeSDL(Game *game)
 {
